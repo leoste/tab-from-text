735be49f1b5eceaacfbbad117f009050cf975b09
leoste <35530198+leoste@users.noreply.github.com>
2026-02-20 00:01:41 +0200

    good work

--- song/jahe.py
+++ song/jahe.py
@@ -6,27 +6,7 @@ from object.Segment import Segment
 from object.GuitarString import GuitarString
 from object.StrumStyle import StrumStyle
 
-intro_rhythm_durations = [2,1,1,2,1,1,1,1,1,2,1,1,1]
-INTRO_RHYTHM = Rhythm(
-    intro_rhythm_durations,
-    [StrumStyle.NORMAL] * len(intro_rhythm_durations)
-)
-
-SALM_RHYTHM = Rhythm(
-    [1] * 16,
-    [StrumStyle.PALM_MUTED] * 16
-)
-
-SALM_HALF_END_RHYTHM = Rhythm(
-    [1,1,1,2,1,1,1] + [1] * 8,
-    [StrumStyle.NORMAL] * 16
-)
-
-SALM_END_RHYTHM = Rhythm(
-    [1,1,1,2,1,1,1] * 2,
-    [StrumStyle.NORMAL] * 16
-)
-
+CHORD_13_F = Chord.get_power_chord(GuitarString.E6, 13)
 CHORD_11_D_SHARP = Chord.get_power_chord(GuitarString.E6, 11)
 CHORD_9_C_SHARP = Chord.get_power_chord(GuitarString.E6, 9)
 CHORD_8_C = Chord.get_power_chord(GuitarString.E6, 8)
@@ -34,6 +14,14 @@ CHORD_6_A_SHARP = Chord.get_power_chord(GuitarString.E6, 6)
 CHORD_4_G_SHARP = Chord.get_power_chord(GuitarString.E6, 4)
 CHORD_2_F_SHARP = Chord.get_power_chord(GuitarString.E6, 2)
 CHORD_1_F = Chord.get_power_chord(GuitarString.E6, 1)
+
+DURATIONS_232 = [1,1,1,2,1,1,1]
+
+intro_rhythm_durations = [2,1,1,2,1,1] + DURATIONS_232
+INTRO_RHYTHM = Rhythm(
+    intro_rhythm_durations,
+    [StrumStyle.NORMAL] * 16
+)
 
 INTRO = Segment(
     "INTRO",
@@ -52,6 +40,23 @@ INTRO = Segment(
             ]
         )
     ]
+)
+
+SALM_RHYTHM = Rhythm(
+    [1] * 16,
+    [StrumStyle.PALM_MUTED] * 16
+)
+
+salm_half_end_durations = DURATIONS_232 + [1] * 8
+SALM_HALF_END_RHYTHM = Rhythm(
+    salm_half_end_durations,
+    [StrumStyle.NORMAL] * 16
+)
+
+salm_end_durations =  DURATIONS_232 * 2
+SALM_END_RHYTHM = Rhythm(
+    salm_end_durations,
+    [StrumStyle.NORMAL] * 16
 )
 
 SALM = Segment(
@@ -83,3 +88,104 @@ SALM = Segment(
         )
     ]
 )
+
+chorus_first_measure_durations = [2] + [1] * 14
+CHORUS_FIRST_MEASURE_RHYTHM = Rhythm(
+        chorus_first_measure_durations,
+        [StrumStyle.NORMAL] * 16
+)
+
+CHORUS_THIRD_MEASURE_RHYTHM = Rhythm(
+        [1] * 16,
+        [StrumStyle.NORMAL] * 16
+)
+
+chorus_seventh_measure_durations = [3,3,2,1,1,1,1,1,1,1,1]
+CHORUS_SEVENTH_MEASURE_RHYTHM = Rhythm(
+    chorus_seventh_measure_durations,
+    [StrumStyle.NORMAL] * 16
+)
+
+
+CHORUS = Segment(
+    "REFRÄÄN",
+    [
+        RhythmicChordSpan(
+            CHORUS_FIRST_MEASURE_RHYTHM,
+            [ ChordSpan(16, CHORD_9_C_SHARP) ]
+        ),
+        RhythmicChordSpan(
+            CHORUS_THIRD_MEASURE_RHYTHM,
+            [
+                ChordSpan(16, CHORD_6_A_SHARP),
+                ChordSpan(16, CHORD_4_G_SHARP)
+            ],
+        ),
+        RhythmicChordSpan(
+            CHORUS_SEVENTH_MEASURE_RHYTHM,
+            [
+                ChordSpan(9, CHORD_9_C_SHARP),
+                ChordSpan(7, CHORD_8_C)
+            ]
+        )
+    ] * 2
+)
+
+# TODO: make same style duration calculation for rhythm strumstyle and chord span.
+# right now chord span is counting halfbeats, but strumstyle actual array members.
+# make both halfbeats. also make it universally understood everywhere that its halfbeats.
+# this gets rid of all the *len() or most of them
+
+break_last_measure_durations = [1] * 8 + DURATIONS_232
+BREAK_LAST_MEASURE_RHYTHM = Rhythm(
+    break_last_measure_durations,
+    [StrumStyle.NORMAL] * 16
+)
+
+BREAK = Segment(
+    "BREAKDOWN",
+    [
+        RhythmicChordSpan(
+            Rhythm(
+                [1] * 16,
+                [StrumStyle.NORMAL] * 16
+            ),
+            [
+                ChordSpan(16, CHORD_13_F),
+                ChordSpan(4, CHORD_9_C_SHARP),
+                ChordSpan(4, CHORD_8_C),
+                ChordSpan(8, CHORD_9_C_SHARP),
+                ChordSpan(16, CHORD_6_A_SHARP),
+                ChordSpan(16, CHORD_8_C),
+                ChordSpan(16, CHORD_13_F),
+                ChordSpan(4, CHORD_9_C_SHARP),
+                ChordSpan(4, CHORD_8_C),
+                ChordSpan(8, CHORD_9_C_SHARP),
+                ChordSpan(16, CHORD_6_A_SHARP)
+            ]
+        ),
+        RhythmicChordSpan(
+            BREAK_LAST_MEASURE_RHYTHM,
+            ChordSpan(16, CHORD_8_C)
+        )
+        # TODO: add 2 measures of silence that has 4 halfbeats long upwards bend
+        # TODO: add support for bends
+    ]
+)
+
+END = [
+    # TODO: add end tabs
+]
+
+SONG = [
+    INTRO,
+    SALM,
+    CHORUS,
+    INTRO,
+    SALM,
+    CHORUS,
+    BREAK,
+    CHORUS,
+    CHORUS,
+    END
+]

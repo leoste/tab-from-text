ebc0169b3948b9a43799e9e2d715815e561be26f
leoste <35530198+leoste@users.noreply.github.com>
2026-04-26 02:55:05 +0300

    add elu meie moodi bass rhythm

---
+++ demo/songs/elu.py
@@ -0,0 +1,210 @@
+from tabfromtext.song.Lyrics import Lyrics
+from tabfromtext.song.RhythmicChordSpanList import RhythmicChordSpanList
+from tabfromtext.song.ChordSpan import ChordSpan
+from tabfromtext.song.Chord import Chord
+from tabfromtext.song.Rhythm import Rhythm
+from tabfromtext.song.Segment import Segment
+from tabfromtext.song.GuitarString import E6, A5, D4, G3, B2, E1
+from tabfromtext.song.StrumStyle import StrumStyle
+from tabfromtext.song.RepeatingChordSpan import RepeatingChordSpan
+from tabfromtext.song.Song import Song
+
+RHYTHM = "Rhythm"
+BASS = "Bass"
+LEAD = "Lead"
+
+rhythm_vahe_parts = [
+    RhythmicChordSpanList(
+        Rhythm(
+            [1] * 14 + [2] +
+            [1] * 8 + [1,2,1,2,2],
+            [StrumStyle.NORMAL] * 8 +
+            [
+                StrumStyle.NORMAL, StrumStyle.NORMAL,
+                StrumStyle.MUTED, StrumStyle.NORMAL,
+                StrumStyle.MUTED, StrumStyle.MUTED,
+            ] +
+            [StrumStyle.NORMAL] * 2 +
+            [StrumStyle.NORMAL] * 16
+        ),
+        [
+            ChordSpan(8, Chord.power_chord(E6, 7)),
+            ChordSpan(8, Chord.power_chord(E6, 6)),
+            ChordSpan(4, Chord.power_chord(A5, 6)),
+            ChordSpan(4, Chord.power_chord(E6, 6)),
+            ChordSpan(6, Chord.power_chord(A5, 6)),
+            ChordSpan(2, Chord.power_chord(E6, 7))
+        ] * 2
+    )
+]
+
+bass_vahe_parts = [
+    RepeatingChordSpan(1, Chord.single_note(E6, 7), 8),
+    RepeatingChordSpan(1, Chord.single_note(E6, 6), 8),
+    RepeatingChordSpan(1, Chord.single_note(A5, 6), 4),
+    RepeatingChordSpan(1, Chord.single_note(E6, 6), 4),
+    RepeatingChordSpan(1, Chord.single_note(A5, 6), 4),
+    RepeatingChordSpan(1, Chord.single_note(E6, 7), 3),
+    RepeatingChordSpan(1, Chord.single_note(A5, 6))
+] * 2
+
+rhythm_salm_first_parts = [
+    RepeatingChordSpan(1, Chord.single_note(E6, 6), 4),
+    RepeatingChordSpan(1, Chord.power_chord(E6, 7), 3),
+    RepeatingChordSpan(1, Chord.power_chord(E6, 6)),
+    RepeatingChordSpan(1, Chord.single_note(E6, 7), 4),
+    RepeatingChordSpan(1, Chord.power_chord(E6, 7), 3),
+    RepeatingChordSpan(1, Chord.power_chord(E6, 6)),
+]
+
+rhythm_salm_parts = rhythm_salm_first_parts + [
+    RepeatingChordSpan(1, Chord.single_note(A5, 6), 4),
+    RepeatingChordSpan(1, Chord.power_chord(A5, 6)),
+    RepeatingChordSpan(1, Chord.single_note(A5, 6)),
+    RepeatingChordSpan(1, Chord.power_chord(A5, 6)),
+    RepeatingChordSpan(1, Chord.single_note(A5, 6)),
+    RepeatingChordSpan(1, Chord.single_note(A5, 6), 4),
+    RepeatingChordSpan(1, Chord.power_chord(E6, 7), 3),
+    RepeatingChordSpan(1, Chord.power_chord(E6, 6)),
+] + rhythm_salm_first_parts + [
+    RepeatingChordSpan(1, Chord(None, None, None, 13, 11, 11), 8),
+    RepeatingChordSpan(1, Chord.power_chord(E6, 11)),
+    RepeatingChordSpan(1, Chord(None, None, None, None, 11, 11)),
+    RepeatingChordSpan(1, Chord.power_chord(E6, 11)),
+    RepeatingChordSpan(1, Chord(None, None, None, None, 11, 11)),
+    RepeatingChordSpan(1, Chord.power_chord(E6, 11), 3),
+    RepeatingChordSpan(1, Chord(None, None, None, None, 11, 11)),
+]
+
+bass_salm_first_parts = [
+    RepeatingChordSpan(1, Chord.single_note(E6, 6), 6),
+    RepeatingChordSpan(1, Chord.single_note(E6, 7)),
+    RepeatingChordSpan(1, Chord.single_note(E6, 6)),
+    RepeatingChordSpan(1, Chord.single_note(E6, 7), 7),
+    RepeatingChordSpan(1, Chord.single_note(E6, 6)),
+    RepeatingChordSpan(1, Chord.single_note(A5, 6), 8),
+]
+
+bass_salm_parts = bass_salm_first_parts + [
+    RepeatingChordSpan(1, Chord.single_note(A5, 6), 6),
+    RepeatingChordSpan(1, Chord.single_note(E6, 7)),
+    RepeatingChordSpan(1, Chord.single_note(E6, 6))
+] + bass_salm_first_parts + [    
+    RepeatingChordSpan(2, Chord.single_note(A5, 9)),
+    RepeatingChordSpan(2, Chord.single_note(E6, 6)),
+    RepeatingChordSpan(1, Chord.single_note(E6, 6)),
+    RepeatingChordSpan(1, Chord.single_note(D4, 3)),
+    RepeatingChordSpan(1, Chord.single_note(D4, 4), 2),
+]
+
+rhythm_ref_parts = [
+    RhythmicChordSpanList(
+        Rhythm(
+            [2, 1,1,1,1,1,1] +
+            [1,1,1,1,1,1,1,1] * 3,
+            [StrumStyle.NORMAL] * 8 +
+            ([StrumStyle.NORMAL] * 5 + [StrumStyle.NO_HIT, StrumStyle.NORMAL, StrumStyle.NO_HIT]) * 3
+        ),
+        [
+            ChordSpan(8, Chord.power_chord(E6, 2)),
+            ChordSpan(4, Chord.power_chord(E6, 7)),
+            ChordSpan(8, Chord.power_chord(E6, 6)),
+            ChordSpan(8, Chord.power_chord(A5, 6)),
+            ChordSpan(4, Chord.power_chord(E6, 6))
+        ] * 2
+    )
+]
+
+bass_ref_parts = [
+    RepeatingChordSpan(1, Chord.single_note(D4, 4), 8),
+    RepeatingChordSpan(1, Chord.single_note(E6, 7), 4),
+    RepeatingChordSpan(1, Chord.single_note(E6, 6), 8),
+    RepeatingChordSpan(1, Chord.single_note(A5, 6), 8),
+    RepeatingChordSpan(1, Chord.single_note(E6, 6), 4)
+] * 2
+
+rhythm_outro_parts = [
+    RhythmicChordSpanList(
+        Rhythm([1]),
+        [
+            ChordSpan(6, Chord.power_chord(A5, 6)),
+            ChordSpan(2, Chord.power_chord(E6, 4)),
+            ChordSpan(4, Chord.power_chord(E6, 6)),
+            ChordSpan(1, Chord.power_chord(E6, 1)),
+            ChordSpan(1, Chord.power_chord(E6, 2)),
+            ChordSpan(1, Chord.power_chord(A5, 2)),
+            ChordSpan(1, Chord.power_chord(A5, 1)),
+            ChordSpan(4, Chord.power_chord(A5, 1)),
+            ChordSpan(4, Chord.power_chord(E6, 6)),
+            ChordSpan(3, Chord.power_chord(E6, 7)),
+            ChordSpan(1, Chord.power_chord(E6, 6)),
+            ChordSpan(3, Chord.power_chord(A5, 2)),
+            ChordSpan(1, Chord.power_chord(A5, 1)),
+        ] * 4
+    )
+]
+
+bass_outro_parts = [
+    RepeatingChordSpan(1, Chord.single_note(E6, 7), 8),
+    RepeatingChordSpan(1, Chord.single_note(E6, 6), 8),
+    RepeatingChordSpan(1, Chord.single_note(A5, 6), 4),
+    RepeatingChordSpan(1, Chord.single_note(G3, 8), 4),
+    RepeatingChordSpan(1, Chord.single_note(A5, 6), 4),
+    RepeatingChordSpan(1, Chord.single_note(E6, 7), 3),
+    RepeatingChordSpan(1, Chord.single_note(A5, 6))
+] * 4
+
+VAHE = Segment("INTRO", {
+    RHYTHM: rhythm_vahe_parts,
+    BASS: bass_vahe_parts
+})
+
+SALM_1 = Segment("1. SALM", {
+    RHYTHM: rhythm_salm_parts,
+    BASS: bass_salm_parts
+})
+
+SALM_2 = Segment("2. SALM", {
+    RHYTHM: rhythm_salm_parts,
+    BASS: bass_salm_parts
+})
+
+REF = Segment("REFRÄÄN", {
+    RHYTHM: rhythm_ref_parts,
+    BASS: bass_ref_parts
+})
+
+REF_2X = Segment("TOPELT REFRÄÄN", {
+    RHYTHM: rhythm_ref_parts * 2,
+    BASS: bass_ref_parts * 2
+})
+
+OUTRO = Segment("OUTRO", {
+    RHYTHM: rhythm_outro_parts,
+    BASS: bass_outro_parts
+})
+
+END = Segment("LÕPP", {
+    RHYTHM: [
+        RepeatingChordSpan(2, Chord.single_note(E6, 6), 1, StrumStyle.SLIDE),
+        RepeatingChordSpan(6, Chord.single_note(E6, 18), 1, StrumStyle.NORMAL),
+        RepeatingChordSpan(2, Chord.power_chord(E6, 6)),
+        RepeatingChordSpan(6, None)
+    ],
+    BASS: [
+        RepeatingChordSpan(8, Chord.single_note(D4, 8)),
+        RepeatingChordSpan(2, Chord.single_note(E6, 6)),
+        RepeatingChordSpan(6, None)
+    ]
+})
+
+SONG = Song("Elu meie moodi", [
+    VAHE,
+    SALM_1,
+    REF,
+    VAHE,
+    SALM_2,
+    REF_2X,
+    OUTRO,
+    END
+])
--- main.py
+++ main.py
@@ -3,10 +3,12 @@ from demo.songs.song4243 import SONG as SONG_4243
 from demo.songs.viirastus import SONG as VIIRASTUS
 from demo.songs.ilmutus import SONG as ILMUTUS
 from demo.songs.toukerattur import SONG as TOUKERATTUR
+from demo.songs.elu import SONG as ELU
 from tabfromtext.render.TabPrinter import print_song
 
-print_song(SONG_4243, "demo/tabs")
-print_song(JAHE, "demo/tabs")
-print_song(VIIRASTUS, "demo/tabs")
-print_song(ILMUTUS, "demo/tabs")
-print_song(TOUKERATTUR, "demo/tabs")
+#print_song(SONG_4243, "demo/tabs")
+#print_song(JAHE, "demo/tabs")
+#print_song(VIIRASTUS, "demo/tabs")
+#print_song(ILMUTUS, "demo/tabs")
+#print_song(TOUKERATTUR, "demo/tabs")
+print_song(ELU, "demo/tabs")
